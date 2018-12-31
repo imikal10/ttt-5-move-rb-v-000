cdbaf3386a73df96a5e9f4add4ab9ddd4a1a81c2
Isaiah Magett <imagett10@gmail.com>
2018-12-31 00:22:08 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +7,18 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 # code your input_to_index and move method here!
+# this simple task took me more time than I ever expected.  It is here to note that can give my argument mutiple characteristics by added the .function.  I could repeat the argument 10 times is I wanted to_i
+def input_to_index(user_input)
+  user_input.to_i - 1 
+end
+
+def move(board, position, char = "X" )
+  board[position]=char
+end
+# move(board,5,"X")
+
+
+
